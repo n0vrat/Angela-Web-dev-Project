95572ec6882274e09b90fd60c77729a15cb51e79
n0vrat <66128128+n0vrat@users.noreply.github.com>
2025-04-13 16:08:36 +0800

    added Javascript Exercises folder

---
+++ JavaScript Exercises/whosPaying.js
@@ -0,0 +1,17 @@
+var list = ["Angela", "Ben", "Jenny", "Michael", "Chloe"];
+
+function whosPaying(names) {
+    
+/******Don't change the code above*******/
+    
+    //Write your code here.
+  var whoPays = Math.floor(Math.random() * names.length); 
+  console.log(names[whoPays] + " is going to buy lunch today!"); 
+    
+    
+
+
+/******Don't change the code below*******/    
+}
+
+whosPaying(list);
